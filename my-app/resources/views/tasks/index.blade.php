7474280fd3f4fb232a9f2e564ae4795a0ab15845
Aoi <a.sakamoto.pr@gmail.com>
2025-06-06 10:36:55 +0900

    Implemented task completion function

--- my-app/resources/views/tasks/index.blade.php
+++ my-app/resources/views/tasks/index.blade.php
@@ -9,23 +9,40 @@
 </head>
 <body class="bg-gray-100">
 
-    <div class="container mx-auto p-4">
-        <nav class="flex justify-between">
+    <div class="container mx-auto p-4 md:w-6/12">
+        <nav class="flex justify-between md:justify-normal">
             <h1 class="text-2xl font-bold mb-4">Todoリスト</h1>
             <a href="{{ route('tasks.trash') }}" class="bg-gray-300 p-2 mb-2 rounded">ゴミ箱</a>
         </nav>
         
         <form action="{{ route('tasks.store')}}" method="POST" class="mb-4">
             @csrf
-            <div class="flex mb-2">
-                <input type="text" name="task_name" class="border border-gray-300 w-full">
-                <input type="datetime-local" name="due_date" class="border p-2 ml-2">
+            <div class="mb-2">
+                <div>
+                    <div>
+                        <label for="task_name" class="text-xs">タスク</label>
+                    </div>
+                    <input type="text" name="task_name" id="task_name" class="border border-gray-300 w-48 mb-2">
+                </div>
+                <div>
+                    <div>
+                        <label for="due_date" class="text-xs">期限</label>
+                    </div>
+                    <input type="datetime-local" name="due_date" id="due_date" value="{{ now()->format('Y-m-d H:i') }}" class="border p-2 mb-2">
+                </div>
                 <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">追加</button>
             </div>
         </form>
-        <ul>
+        <ul class="list-disc mx-auto pl-5 text-sm md:w-6/12">
             @foreach ($tasks as $task)
-                <li>{{ $task->task_name }}</li>
+                <li class="flex justify-between md:justify-normal mb-2">
+                    <div>{{ $task->task_name }} : <span class="mx-1">{{ date('Y-m-d H:i', strtotime($task->due_date)) }}</span></div>
+                    <form action="{{ route('tasks.maskAsDelete', $task->id) }}" method="POST">
+                        @csrf
+                        @method('PUT')
+                        <button type="submit" class="bg-green-500 text-white px-2 py-1 rounded">Done!</button>
+                    </form>
+                </li>
             @endforeach
         </ul>
     </div>
