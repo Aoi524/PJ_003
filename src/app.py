3aaa589e194622223b651ced6d7cd7565f43c98c
Aoi <a.sakamoto.pr@gmail.com>
2025-08-04 17:54:45 +0900

    データベースを実装

--- src/app.py
+++ src/app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 app = Flask(__name__)
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://mysql:mysql@booklist-db:3306/BookLists'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    author = db.Column(db.String(100), nullable=False)
+    publisher = db.Column(db.String(100), nullable=True)
+    published_date = db.Column(db.Date, nullable=False)
+    isbn = db.Column(db.String(20), unique=True, nullable=True)
+    status = db.Column(db.String(50), nullable=False, default='未読')
+    readed_date = db.Column(db.Date, nullable=True)
+
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
